d3f8d3f9f0ca86d92a91b07997d812d818e7bfe4
Jukka Lankinen <jukka.lankinen@gmail.com>
2013-01-27 13:30:30 +0200

    Added the missing files

---
+++ src/scenes/gameover.js
@@ -0,0 +1,22 @@
+
+Crafty.scene("gameover", function() {
+
+	var elements = [
+        /*"src/interfaces/info.js",
+        "src/entities/tile.js",
+        "src/interfaces/title_text.js",
+        "src/interfaces/title_bird.js",
+        "src/interfaces/new_game.js",
+        "src/interfaces/heart_icon.js",
+        "src/interfaces/score_text.js",*/
+        //"src/interfaces/back_to_menu.js",
+        //"src/interfaces/restart_game.js",
+	];
+	
+	//when everything is loaded, run the main scene
+	require(elements, function() {	   
+		
+
+	});
+
+});
